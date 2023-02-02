394c0e591344bc8a324f4a4ed2c927d805265cc4
blackhole <10030358+strangerover2002@users.noreply.github.com>
2023-02-02 19:44:55 +0000

    remove unwanted files

--- uboot_include/u74.sh
+++
@@ -1,3 +0,0 @@
-./bin/openocd -f /home/user/sandpit/openocd-bin/share/openocd/scripts/interface/jlink.cfg  \
-    -c "transport select jtag" -c "adapter_khz 1000"  \
-    -f /home/user/sandpit/openocd-bin/u74.cfg
